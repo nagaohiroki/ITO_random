e22f96bfe8f2b5827eb50cb3ff19531e06d11ba1
nagaohiroki <testplayers0226@gmail.com>
2022-03-20 21:23:21 +0900

    add discord bot

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # ITO_random
+
+this is discord bot.
+
+### Launch
+
+```
+$python ito.py
+```
+
+### Message
+
+a card
+
+```
+/ito
+```
+
+two cards
+
+```
+/ito2
+```
--- ito.py
+++ ito.py
@@ -1,11 +1,46 @@
+import discord
 import random
-card = range(1, 101)
-card = random.sample(card, len(card))
-print(card)
-card_num = 2
-player_num = 5
-for i in range(0, player_num):
-    txt = str(card[0:card_num])
-    card = card[card_num:]
-    file = open(str(i) + ".txt", "w")
-    file.write(txt)
+
+
+token = ''
+command = '/ito'
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+
+def ito_random(players, card_num):
+    card = range(1, 101)
+    rand = random.sample(card, len(card))
+    txt = ''
+    i = 0
+    for player in players:
+        num = rand[card_num * i : card_num * (i + 1)]
+        txt += f'<@{player}> ||{num}||\n'
+        i += 1
+    return txt
+
+
+@client.event
+async def on_ready():
+    print('ITO bot login')
+
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    content = message.content
+    if content.startswith(command) == False:
+        return
+    num = 1
+    num_str = content.replace(command, '')
+    if num_str != '':
+        num = int(num_str)
+    entry_member = []
+    for member in message.guild.members:
+        if member.bot == False and member.status == discord.Status.online:
+            entry_member.append(member.id)
+    await message.channel.send(ito_random(entry_member, num))
+
+
+client.run(token)
